08de483e1c2cd2cb7cf1c4dac845b9b2dfc8f6bf
Hunter-c12 <124923393+Hunter-c12@users.noreply.github.com>
2023-02-20 01:09:59 +0300

    Add files via upload

---
+++ DbVk.py
@@ -0,0 +1,9 @@
+TOKEN_GROUP = '' # токен пользователя
+TOKEN_USER = '' # токен сообщества
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1'
+user = 'postgres'
+password = 'hunterc12'
+db_name = 'bot_users'
